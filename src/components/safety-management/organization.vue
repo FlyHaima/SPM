638eb08d1ba33b24d561a1e1bd884a5ec9ad8a70
FlyHaima <2207252182@qq.com>
2019-10-14 11:43:30 +0800

    add files; change router

---
+++ src/components/safety-management/organization.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Organization
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'organization'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/safety-management/readme.md
@@ -0,0 +1,6 @@
+## 这个文件夹内为“安全基础管理”模块
+具体分为以下四个模块
++ 组织机构 （Organization）
++ 全员培训 （StaffTraining）
++ 制度建设 （SystemConstruction）
++ 基础资料 （BasicData）
