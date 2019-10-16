f75de04b69af43cce517d1f477d6c43a8b5412da
FlyHaima <2207252182@qq.com>
2019-10-16 14:08:48 +0800

    commit files： tree

--- src/components/tree-diagram/treeDiagram.vue
+++ src/components/tree-diagram/treeDiagram.vue
@@ -14,12 +14,49 @@
     <div class="tree-box">
       <el-tree
         class="filter-tree"
-        :data="data"
+        :data="treeData"
         :props="defaultProps"
         default-expand-all
+        node-key="id"
         :filter-node-method="filterNode"
+        :expand-on-click-node="false"
+        @node-click="handleNodeClick"
         ref="tree">
+        <span class="custom-tree-node" slot-scope="{ node, data }" :title="node.label">
+          <span>{{ node.label }}</span>
+          <span class="right-btns">
+            <i class="el-icon-plus" @click="() => append(data)"></i>
+            <i class="el-icon-edit"></i>
+            <i class="el-icon-delete"  @click="() => remove(node, data)"></i>
+          </span>
+        </span>
       </el-tree>
+      <!--
+      <el-tree
+        :data="data"
+        show-checkbox
+        node-key="id"
+        default-expand-all
+        :expand-on-click-node="false">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+          <span>{{ node.label }}</span>
+          <span>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => append(data)">
+              Append
+            </el-button>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => remove(node, data)">
+              Delete
+            </el-button>
+          </span>
+        </span>
+      </el-tree>
+      -->
     </div>
   </div>
 </template>
@@ -30,27 +67,27 @@ export default {
   data () {
     return {
       filterText: '',
-      data: [{
+      treeData: [{
         id: 1000131,
-        label: '黑龙江总公司',
+        label: '《安全生产管理平台》东三省黑龙江分部总公司',
         children: [{
-          id: 4,
+          id: 1003422,
           label: '安管部',
           children: [{
-            id: 9,
+            id: 1004521,
             label: '检查组'
           }, {
-            id: 10,
+            id: 1004522,
             label: '设备组'
           }]
         }, {
-          id: 4,
+          id: 1000135,
           label: '生产部',
           children: [{
-            id: 9,
+            id: 1060121,
             label: '生产A组'
           }, {
-            id: 10,
+            id: 1060122,
             label: '生产B组'
           }]
         }]
@@ -65,10 +102,29 @@ export default {
     filterNode (value, data) {
       if (!value) return true
       return data.label.indexOf(value) !== -1
+    },
+    handleNodeClick (data) {
+      // 点击切换节点
+      // console.log(data)
+    },
+    remove (node, data) {
+      const parent = node.parent
+      const children = parent.data.children || parent.data
+      const index = children.findIndex(d => d.id === data.id)
+      children.splice(index, 1)
+    },
+    append (data) {
+      const newChild = { label: 'testtest', children: [] }
+      if (!data.children) {
+        this.$set(data, 'children', [])
+      }
+      console.log(data)
+      data.children.push(newChild)
     }
   },
   watch: {
     filterText (val) {
+      console.log(this.$refs.tree)
       this.$refs.tree.filter(val)
     }
   }
@@ -118,6 +174,24 @@ export default {
           font-size: 12px;
         }
       }
+    }
+  }
+  .tree-box{
+    padding: 6px 4px;
+    overflow: auto;
+    .custom-tree-node{
+      display: inline-block;
+      width: 15em;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      .right-btns{
+        position: absolute;
+        right: 8px;
+      }
+    }
+    .el-tree-node__content{
+      margin-top: 10px;
     }
   }
 }
