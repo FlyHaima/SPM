75d78124e3b65270cd202eca9d55d6f1c4af998d
FlyHaima <2207252182@qq.com>
2019-10-17 17:26:41 +0800

    commit files： tree

--- src/components/tree-diagram/treeDiagram.vue
+++ src/components/tree-diagram/treeDiagram.vue
@@ -1,7 +1,8 @@
 <!-- 树形结构图（右） -->
 <!-- 说明：
-  -- 1、level：用于规定最多可添加到第几层级,如果在最后一级添加节点，会变成添加兄弟节点；
-  -- 2、showEditor：是否显示层级编辑按钮区域（关联权限）
+  -- 1、level：用于规定最多可添加到第几层级；
+  -- 2、addBro：如果在最后一级添加节点，会变成添加兄弟节点；
+  -- 3、showEditor：是否显示层级编辑按钮区域（关联权限）
  -->
 <template>
   <div class="tree-diagram">
@@ -29,9 +30,9 @@
         <span class="custom-tree-node" slot-scope="{ node, data }" :title="node.label">
           <span>{{ node.label }}</span>
           <span class="right-btns">
-            <i class="el-icon-plus" title="添加节点" @click="() => append(node, data)"></i>
+            <i class="el-icon-plus" title="添加节点" @click="append(node, data)"></i>
             <i class="el-icon-edit" title="修改节点" @click="edit(node)"></i>
-            <i class="el-icon-delete" title="删除节点"  @click="() => remove(node, data)"></i>
+            <i class="el-icon-delete" title="删除节点"  @click="remove(node, data)"></i>
           </span>
         </span>
       </el-tree>
@@ -53,7 +54,10 @@ export default {
           label: '安管部',
           children: [{
             id: 1004521,
-            label: '检查组'
+            label: '检查组',
+            data: {
+              name: ''
+            }
           }, {
             id: 1004522,
             label: '设备组'
@@ -74,7 +78,8 @@ export default {
         children: 'children',
         label: 'label'
       },
-      level: 3
+      level: 3,
+      addBro: false
     }
   },
   methods: {
@@ -85,17 +90,8 @@ export default {
     handleNodeClick (data) { // 点击节点，切换右侧结构视图
       // this.$emit('open-loading')
     },
-    remove (node, data) {
-      const parent = node.parent
-      const children = parent.data.children || parent.data
-      const index = children.findIndex(d => d.id === data.id)
-      children.splice(index, 1)
-    },
-    edit (node) {
-      console.log(node)
-      node.data.label = 'new'
-    },
     append (node, data) {
+      this.openAppendBox()
       if (node.level < this.level) {
         const newChild = { label: 'testtest', children: [] }
         if (!data.children) {
@@ -103,9 +99,74 @@ export default {
         }
         console.log(data)
         data.children.push(newChild)
+      } else if (this.addBro && node.level === this.level) {
+        const newBro = { label: 'testtest', children: [] }
       } else {
-        console.log('level big')
+        this.$message({
+          message: '最多可添加到第' + this.level + '级',
+          type: 'warning'
+        })
       }
+    },
+    openAppendBox () {
+      this.$prompt('请输入节点名称', '添加节点', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+        // inputPattern: '', // 输入正则
+        // inputErrorMessage: '' // 正则验证错误提示
+      }).then(() => {
+        // 添加ajax
+        this.$message({
+          type: 'success',
+          message: '节点设置成功'
+        })
+      }).catch(() => {
+        // after cancel, do nothing
+      })
+    },
+    edit (node) {
+      this.openEditBox()
+      console.log(node)
+      node.data.label = 'new'
+    },
+    openEditBox () {
+      this.$prompt('请输入节点名称', '添加节点', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+        // inputPattern: '', // 输入正则
+        // inputErrorMessage: '' // 正则验证错误提示
+      }).then(() => {
+        // 添加ajax
+        this.$message({
+          type: 'success',
+          message: '节点设置成功'
+        })
+      }).catch(() => {
+        // after cancel, do nothing
+      })
+    },
+    remove (node, data) {
+      this.confirmRemove()
+      // const parent = node.parent
+      // const children = parent.data.children || parent.data
+      // const index = children.findIndex(d => d.id === data.id)
+      // children.splice(index, 1)
+    },
+    confirmRemove () {
+      this.$prompt('请输入节点名称', '添加节点', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+        // inputPattern: '', // 输入正则
+        // inputErrorMessage: '' // 正则验证错误提示
+      }).then(() => {
+        // 添加ajax
+        this.$message({
+          type: 'success',
+          message: '节点设置成功'
+        })
+      }).catch(() => {
+        // after cancel, do nothing
+      })
     }
   },
   watch: {
