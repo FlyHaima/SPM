14bd654ed294dfaa27cf10bf8b6f5115664e7bd2
Silenter <2207252182@qq.com>
2019-10-27 23:29:24 +0800

    fix bug for remove item from chooseList

--- src/components/tree-diagram/treeTransfer.vue
+++ src/components/tree-diagram/treeTransfer.vue
@@ -34,7 +34,7 @@
       <ul>
         <li class="list-item" v-for="(item, index) in chooseList" :key="index">
           {{item.nameStr}}
-          <i class="el-icon-delete" title="删除" @click="remove(item.id, treeData)"></i>
+          <i class="el-icon-delete" title="删除" @click="remove(item.id)"></i>
         </li>
       </ul>
     </div>
@@ -173,15 +173,26 @@ export default {
       this.screenAppendData(data)
       this.$refs.tree.filter() // 操作后使用filter方法，筛选所有selected 不是 true 的
     },
-    remove (id, rData) {
+    screenDelData (sData, id) {
       let vm = this
-      for (let i = 0; i < rData.length; i++) {
-        if (rData[i].id === id) {
-          rData[i].selected = false
-        } else if (rData[i].children) {
-          vm.screenGetData(rData[i].children)
+      for (let i = 0; i < sData.length; i++) {
+        if (sData[i]['id'] === id) {
+          sData[i]['selected'] = false
+          return
+        } else if (sData[i].children) {
+          vm.screenDelData(sData[i].children, id)
         }
       }
+    },
+    remove (id) {
+      let vm = this
+      vm.screenDelData(vm.treeData, id)
+      for (let i = 0; i < vm.chooseList.length; i++) {
+        if (vm.chooseList[i]['id'] === id) {
+          vm.chooseList.splice(i, 1)
+        }
+      }
+      this.$refs.tree.filter()
     }
   }
 }
@@ -246,6 +257,7 @@ export default {
           position: absolute;
           right: 4px;
           top: 11px;
+          cursor: pointer;
         }
       }
     }
