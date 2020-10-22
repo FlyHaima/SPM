d68e70ef6d4aa84651741c7f160c777e576df306
lwzbb <497752269@qq.com>
2020-10-22 08:57:49 +0800

    提交

--- src/components/dialog/dialogCompare.vue
+++ src/components/dialog/dialogCompare.vue
@@ -26,22 +26,22 @@
               v-model="form.state"
               placeholder="请选择分析类型">
               <el-option
-                v-for="(item, index) in searchTypeOpt"
+                v-for="(item, index) in analysisTypeOpt"
                 :key="index"
-                :label="item.label"
-                :value="item.value"></el-option>
+                :label="item"
+                :value="index"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="层级">
-            <el-select
-              v-model="form.state"
-              placeholder="请选择层级">
-              <el-option
-                v-for="(item, index) in searchTypeOpt"
-                :key="index"
-                :label="item.label"
-                :value="item.value"></el-option>
-            </el-select>
+            <el-cascader
+              v-model="form.deptIds"
+              placeholder="请选择层级"
+              :options="deptData"
+              :props="deptDataProps"
+              collapse-tags
+              clearable
+              @change="cascaderChangeHandle"
+              style="width: 460px"></el-cascader>
           </el-form-item>
           <el-form-item label="查询种类">
             <el-select
@@ -55,43 +55,76 @@
                 :value="index"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="查询时间">
+          <el-form-item label="时间1">
             <span v-show="form.searchType === 0">
               <el-date-picker
-                v-model="form.searchTime1"
-                value-format="yyyy-mm-dd"
-                type="date"></el-date-picker>
-              -
-              <el-date-picker
-                v-model="form.searchTime2"
-                value-format="yyyy-mm-dd"
-                type="date"></el-date-picker>
+                v-model="form.date1"
+                type="date"
+                placeholder="选择日"
+                :picker-options= "pickerDisabled"></el-date-picker>
             </span>
             <span v-show="form.searchType === 1">
               <el-date-picker
-                v-model="form.searchTime1"
-                type="month"
-                value-format="mm"></el-date-picker>
-              -
-              <el-date-picker
-                v-model="form.searchTime2"
-                type="month"
-                value-format="mm"></el-date-picker>
+                v-model="form.searchMonth1"
+                type="monthrange"
+                placeholder="选择月"
+                :picker-options= "pickerDisabled"></el-date-picker>
             </span>
             <span v-show="form.searchType === 2">
               <el-date-picker
-                v-model="form.searchTime1"
+                v-model="form.beginYear1"
                 type="year"
-                value-format="yyyy"></el-date-picker>
+                value-format="yyyy"
+                placeholder="选择年"
+                :picker-options= "pickerOptionsBeginYear1"></el-date-picker>
               -
               <el-date-picker
-                v-model="form.searchTime2"
+                v-model="form.endYear1"
                 type="year"
-                value-format="yyyy"></el-date-picker>
+                value-format="yyyy"
+                placeholder="选择年"
+                :picker-options= "pickerOptionsEndYear1"></el-date-picker>
             </span>
             <span v-show="form.searchType === 3">
               <el-date-picker
-                v-model="searchTime"
+                v-model="searchTime1"
+                type="daterange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"></el-date-picker>
+            </span>
+          </el-form-item>
+          <el-form-item label="时间2">
+            <span v-show="form.searchType === 0">
+              <el-date-picker
+                v-model="form.date2"
+                type="date"
+                placeholder="选择日"></el-date-picker>
+            </span>
+            <span v-show="form.searchType === 1">
+              <el-date-picker
+                v-model="form.searchMonth2"
+                type="monthrange"
+                placeholder="选择月"></el-date-picker>
+            </span>
+            <span v-show="form.searchType === 2">
+              <el-date-picker
+                v-model="form.beginYear2"
+                type="year"
+                value-format="yyyy"
+                placeholder="选择年"
+                :picker-options= "pickerOptionsBeginYear2"></el-date-picker>
+              -
+              <el-date-picker
+                v-model="form.endYear2"
+                type="year"
+                value-format="yyyy"
+                placeholder="选择年"
+                :picker-options= "pickerOptionsEndYear2"></el-date-picker>
+            </span>
+            <span v-show="form.searchType === 3">
+              <el-date-picker
+                v-model="searchTime2"
                 type="daterange"
                 range-separator="至"
                 start-placeholder="开始日期"
@@ -111,45 +144,38 @@
               @click="exportHandler">导出</el-button>
           </el-form-item>
         </el-form>
-        <!-- <el-row class="form-group-btn">
-          <el-col>
-            <el-form-item>
-              <el-button
-                type="primary"
-                native-type="submit"
-              >查询</el-button>
-            </el-form-item>
-            <el-form-item>
-              <el-button
-                class="export-btn"
-                type="success"
-                @click="exportHandler">导出</el-button>
-            </el-form-item>
-          </el-col>
-        </el-row> -->
-        <el-table
-          :data="tableData"
-          border
-          style="width: 100%">
-          <el-table-column
-            prop="date"
-            v-for="(item, index) in tableData"
-            :key="index"
-            align="center">
-            <template slot="header" slot-scope="scope">
-               {{tableHeaderData[scope.$index]}}
-            </template>
-            <template slot-scope="scope">
-              {{scope.row[scope.$index]}}
-            </template>
-          </el-table-column>
-        </el-table>
+        <el-tabs type="border-card">
+          <el-tab-pane label="重大风险">
+            <el-table
+              :data="tableData"
+              border
+              style="width: 100%">
+              <el-table-column
+                prop="date"
+                v-for="(item, index) in tableHeaderData"
+                :key="index"
+                align="center">
+                <template slot="header">
+                  {{tableHeaderData[index]}}
+                </template>
+                <template slot-scope="scope">
+                  {{scope.row[index]}}
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="配置管理" name="2">
+            <line-chart :chart="chartLineChart"></line-chart>
+          </el-tab-pane>
+        </el-tabs>
       </div>
     </div>
   </el-dialog>
 </template>
 
 <script>
+import axios from '@/api/axios'
+import lineChart from '@/components/e-charts/lineChart.vue'
 export default {
   name: 'treeDiagram',
   props: {
@@ -164,22 +190,133 @@ export default {
   },
   data () {
     return {
+      activeName: '1',
+      chartLineChart: {
+        height: '400px',
+        legend: [
+          '7月隐患发生率', '7月全员参与率',
+          '8月隐患发生率', '8月全员参与率',
+          '9月隐患发生率', '9月全员参与率',
+          '10月隐患发生率', '10月全员参与率'
+        ],
+        data: [
+          {
+            data1: 120,
+            data2: 132,
+            data3: 101,
+            data4: 134,
+            data5: 120,
+            data6: 132,
+            data7: 101,
+            data8: 134,
+            xAxisdata: '7.1/7.1'
+          },
+          {
+            data1: 8,
+            data2: 20,
+            data3: 8,
+            data4: 18,
+            data5: 8,
+            data6: 20,
+            data7: 8,
+            data8: 18,
+            xAxisdata: '8.1/8.1'
+          },
+          {
+            data1: 120,
+            data2: 132,
+            data3: 101,
+            data4: 134,
+            data5: 120,
+            data6: 132,
+            data7: 101,
+            data8: 134,
+            xAxisdata: '9.1/9.1'
+          },
+          {
+            data1: 8,
+            data2: 20,
+            data3: 8,
+            data4: 18,
+            data5: 8,
+            data6: 20,
+            data7: 8,
+            data8: 18,
+            xAxisdata: '10.1/10.1'
+          }
+        ]
+      },
       submitting: false,
       show: false,
-      searchTime: '',
+      searchTime1: '',
+      searchTime2: '',
+      searchMonth1: '',
+      searchMonth2: '',
       form: {
         state: '',
+        deptIds: '', // 层级
+        deptIdStr: '', // 层级
         searchType: 0, // 查询种类
-        searchTime1: '',
-        searchTime2: '',
-        beginTime: '',
-        endTime: ''
+        date1: '',
+        beginMonth1: '',
+        endMonth1: '',
+        beginYear1: '',
+        endYear1: '',
+        beginTime1: '',
+        endTime1: '',
+        date2: '',
+        beginMonth2: '',
+        endMonth2: '',
+        beginYear2: '',
+        endYear2: '',
+        beginTime2: '',
+        endTime2: ''
       },
-      tableHeaderData: ['列1', '列2', '列3'],
+      pickerDisabled: {
+        // 验证时间范围：不能大于今天
+        disabledDate: (time) => {
+          return time.getTime() > Date.now() - 8.64e6
+        }
+      },
+      pickerOptionsBeginYear1: {
+        disabledDate: time => { // 禁止选择日期大于开始日期
+          const endTime = this.form.endYear1
+          return time.getTime() > new Date(endTime).getTime() - 8.64e7 ||
+                 time.getTime() > Date.now() - 8.64e6
+        }
+      },
+      pickerOptionsEndYear1: {
+        disabledDate: time => { // 禁止选择日期小于开始日期
+          const startTime = this.form.beginYear1
+          return time.getTime() < new Date(startTime).getTime() ||
+                 time.getTime() > Date.now() - 8.64e6
+        }
+      },
+      pickerOptionsBeginYear2: {
+        disabledDate: time => { // 禁止选择日期大于开始日期
+          const endTime = this.form.endYear2
+          return time.getTime() > new Date(endTime).getTime() - 8.64e7 ||
+                 time.getTime() > Date.now() - 8.64e6
+        }
+      },
+      pickerOptionsEndYear2: {
+        disabledDate: time => { // 禁止选择日期小于开始日期
+          const startTime = this.form.beginYear2
+          return time.getTime() < new Date(startTime).getTime() ||
+                 time.getTime() > Date.now() - 8.64e6
+        }
+      },
+      tableHeaderData: ['时间1', '已参与人数', '总人数', '全员参与率', '时间2', '已参与人数', '总人数', '全员参与率', '同比增减'],
       tableData: [
-        ['2016-05-02', '王小虎', 'abc'],
-        ['2016-05-03', '王小虎1', 'bcd'],
-        ['2016-05-04', '王小虎2', 'efg']
+        ['2016-05-03', '23', '45', '5%', '2016-05-03', '34', '78', '5%', '6%'],
+        ['2016-05-04', '25', '40', '5%', '2016-05-03', '34', '78', '5%', '6%'],
+        ['2016-05-05', '27', '48', '5%', '2016-05-03', '34', '78', '5%', '6%'],
+        ['2016-05-03', '23', '45', '5%', '2016-05-03', '34', '78', '5%', '6%'],
+        ['2016-05-04', '25', '40', '5%', '2016-05-03', '34', '78', '5%', '6%'],
+        ['2016-05-05', '27', '48', '5%', '2016-05-03', '34', '78', '5%', '6%'],
+        ['2016-05-03', '23', '45', '5%', '2016-05-03', '34', '78', '5%', '6%'],
+        ['2016-05-04', '25', '40', '5%', '2016-05-03', '34', '78', '5%', '6%'],
+        ['2016-05-05', '27', '48', '5%', '2016-05-03', '34', '78', '5%', '6%']
       ],
       searchTypeOpt: [
         '按日查询',
@@ -187,24 +324,65 @@ export default {
         '按年查询',
         '按时间段查询'
       ],
-      searchTypeOpt1: [
-        {
-          label: '按日期查询',
-          value: 0
-        },
-        {
-          label: '按月份查询',
-          value: 1
-        },
-        {
-          label: '按年度查询',
-          value: 2
-        },
-        {
-          label: '按时间区间查询',
-          value: 3
-        }
-      ]
+      analysisTypeOpt: [
+        '全员参与率',
+        '隐患发生率',
+        '隐患符合率',
+        '隐患整改率'
+      ],
+      deptDataProps: {
+        multiple: true,
+        label: 'invDeptName',
+        value: 'invDeptId'
+      },
+      selectCascadersData: [],
+      deptData: [{
+        value: 1,
+        label: '东南',
+        children: [{
+          value: 2,
+          label: '上海',
+          children: [
+            { value: 3, label: '普陀' },
+            { value: 4, label: '黄埔' },
+            { value: 5, label: '徐汇' }
+          ]
+        }, {
+          value: 7,
+          label: '江苏',
+          children: [
+            { value: 8, label: '南京' },
+            { value: 9, label: '苏州' },
+            { value: 10, label: '无锡' }
+          ]
+        }, {
+          value: 12,
+          label: '浙江',
+          children: [
+            { value: 13, label: '杭州' },
+            { value: 14, label: '宁波' },
+            { value: 15, label: '嘉兴' }
+          ]
+        }]
+      }, {
+        value: 17,
+        label: '西北',
+        children: [{
+          value: 18,
+          label: '陕西',
+          children: [
+            { value: 19, label: '西安' },
+            { value: 20, label: '延安' }
+          ]
+        }, {
+          value: 21,
+          label: '新疆维吾尔族自治区',
+          children: [
+            { value: 22, label: '乌鲁木齐' },
+            { value: 23, label: '克拉玛依' }
+          ]
+        }]
+      }]
     }
   },
   created () {
@@ -213,16 +391,84 @@ export default {
   methods: {
     // 初始化数据
     initData () {
+      this.getDeptData()
     },
     // 关闭弹框
     closeDialog () {
       this.show = false
     },
+    // tab切换事件
+    clickTab (item) {
+      console.log(item)
+      // this.tables.form.tabType = (Number(item.paneName) + 1) + ''
+    },
+    // 级联选择框选中事件处理，格式化选中的数据
+    cascaderChangeHandle (data) {
+      console.log(data)
+      this.selectCascadersData = []
+      data.forEach(item => {
+        this.selectCascadersData.push(item.slice(-1).toString())
+      })
+      this.selectCascadersData = this.selectCascadersData.toString()
+      this.form.deptIdStr = this.selectCascadersData
+    },
     exportHandler () {
     },
     selectChangeSearchType (value) {
       this.form.searchType = value
+      this.form.beginDate = ''
+      this.form.endDate = ''
+      this.form.beginMonth = ''
+      this.form.endMonth = ''
+      this.form.beginYear = ''
+      this.form.endYear = ''
+      this.form.beginTime = ''
+      this.form.endTime = ''
+    },
+    // 获取table列表
+    fetchList () {
+      alert('cc')
+    },
+    // 获取组织机构树
+    getDeptData () {
+      axios
+        .get('basticHidden/getDeptListSize')
+        .then((res) => {
+          if (res.data.code === 200) {
+            this.deptData = res.data.data
+          } else {
+            this.$message({
+              message: res.data.message,
+              type: 'warning'
+            })
+          }
+        })
+    },
+    /**
+     * 表单查询事件
+     */
+    searchHandler () {
+      console.log(this.form)
+      // form.pageNo = 1
+      // this.tableSearchForm.pageNo = 1
+      const specialKey = ['%', "'", ',', '，', '.', '。']
+      // 特殊字符校验
+      try {
+        Object.keys(this.form).forEach(key => {
+          if (specialKey.includes(this.form[key])) {
+            this.$message.error(
+              '查询条件不能为特殊字符百分号[ % ] 、单引号[ ' + "'" + ' ] 以及中英文标点符号[ ，。. ]'
+            )
+          }
+        })
+      } catch (e) {
+        return
+      }
+      this.fetchList()
     }
+  },
+  components: {
+    lineChart
   },
   watch: {
     dialogVisible (val) {
