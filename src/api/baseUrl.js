bf43f4ccb2e05f37b2ab5043170e4b0350f21d1c
FlyHaima <1063435563@qq.com>
2020-11-03 13:46:05 +0800

    修改此页面bug如下：修改文件下载的根目录域名

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -7,14 +7,14 @@
 // 192.168.137.29 // 测试服务
 // http://1.62.120.34:4447/ // 测试华电外网
 // const baseUrl = 'http://10.192.167.2:8808/spm' // 华电内网
-const baseUrl = 'http://192.168.0.158:8033/spm'
+const baseUrl = 'http://192.168.0.168:8033/spm'
 // const baseUrl = 'http://1.181.47.18:9903/spm'海拉尔外网
 // const baseUrl = 'http://127.0.0.1:8808/spm'海拉尔内网
 // const baseUrl = 'http://1.62.120.34:4447/spm'
 
 // 设置七牛云服务器路径
 const uploadQiniuAdr = 'http://upload-z1.qiniu.com'
-const fileQiniuAddr = 'http://qfx2m2lmn.hb-bkt.clouddn.com/'
+const fileQiniuAddr = 'http://files.hljsafety.com/'
 
 export default {
   baseUrl,
